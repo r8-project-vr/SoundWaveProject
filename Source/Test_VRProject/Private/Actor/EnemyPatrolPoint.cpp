08659ac8d993521695c9e3ce6dad0a1affe1c4d7
nameko334 <25ci0511@jec.ac.jp>
2026-06-17 18:51:26 +0900

    EnemyPatrolPointを作りました

---
+++ Source/Test_VRProject/Private/Actor/EnemyPatrolPoint.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/EnemyPatrolPoint.h"
+#include "Components/SceneComponent.h"
+
+// Sets default values
+AEnemyPatrolPoint::AEnemyPatrolPoint()
+{
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemyPatrolPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyPatrolPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
