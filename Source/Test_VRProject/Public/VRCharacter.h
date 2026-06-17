cb01cfda59d9bfb235134f3a06373e1a0d115c69
Lazytiga <25CI0533@jec.ac.jp>
2026-06-17 11:16:24 +0900

    山下作業フォルダの作成&音波の作成
    左クリックでとりあえず音波を出せます

---
+++ Source/Test_VRProject/Public/VRCharacter.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Materials/MaterialParameterCollection.h"
+#include "Materials/MaterialParameterCollectionInstance.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "NiagaraSystem.h"
+#include "VRCharacter.generated.h"
+
+
+class UMaterialInterface;
+class UNiagaraSystem;
+class UInputAction;
+
+struct FInputActionValue;
+UCLASS()
+class TEST_VRPROJECT_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* CaneAction;
+
+	UPROPERTY(EditAnywhere, Category = "Echo")
+	UMaterialParameterCollection* EchoMPC;
+
+	void EmitEcho(const FInputActionValue& Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	bool bEchoActive = false;
+
+	UPROPERTY(EditAnywhere, Category = "Echo")
+	float CurrentEchoRadius = 0.f;
+
+	UPROPERTY(EditAnywhere, Category = "Echo")
+	float MaxEchoRadius = 1000.f;
+
+	UPROPERTY(EditAnywhere, Category = "Echo")
+	float EchoSpeed = 2000.f;
+
+	UPROPERTY()
+	TSet<UStaticMeshComponent*> RevealedMeshes;
+
+	FVector EchoOrigin;
+
+};
